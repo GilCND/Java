669dd8bd8ff854d8cc9d0b08c6ca4daa7e26e54c
Felipe <69941457+GilCND@users.noreply.github.com>
2020-08-25 16:54:24 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,19 @@
+Program Name: Animal 
+Objective: Practice the println command
+Programmer: Felipe SG
+
+
+import javax.swing.JOptionPane;
+public class AnimalWordBallon {
+public static void main(String[] args) {
+
+System.out.println("    /\\___/\\  |-------------|    ");
+System.out.println("   /_    _ \\  |   Hey You   |   ");
+System.out.println("  ||_|  |_|| |/------------|   ");
+System.out.println("  |  -^-   |  ---------( )   ()    ");
+System.out.println("   --------            ( )  ( ) ");
+System.out.println("      |                /( ) ( )     ");
+System.out.println("      |/-\\_________/-\\/  ( )        ");
+
+}
+}
