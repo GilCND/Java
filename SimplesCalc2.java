18c9a81d0facb2bd7dd9bea9daaa4d8d584d70ff
Felipe <69941457+GilCND@users.noreply.github.com>
2020-08-25 16:49:58 -0300

    Create SimplesCalc2.java

---
+++ SimplesCalc2.java
@@ -0,0 +1,13 @@
+Program name: Simple Calc 2
+Objective: Calculate Square feet. 
+Programmer: Felipe SG
+
+import javax.swing.JOptionPane;
+public class YardWork {
+	public static void main(String[] args) {
+		double length  = 15;
+		double width  = 25;
+		double result4 = length*width;
+		System.out.println("You have" + result4 + "square feet to mow");
+	}
+}
