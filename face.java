0f345e8859930a9eba74cbb544c9eaf1517e8b5f
Felipe <69941457+GilCND@users.noreply.github.com>
2020-08-25 16:53:12 -0300

    Create face.java

---
+++ face.java
@@ -0,0 +1,19 @@
+Program Name: Face
+Objective: Practice the println command. 
+Programmer: Felipe SG 
+
+
+import javax.swing.JOptionPane;
+public class SimilarFace {
+public static void main(String[] args) {
+
+System.out.println("   _//////_  ");
+System.out.println("  | _    _ | ");
+System.out.println("  ||_|  |_|| ");
+System.out.println("  |   <>   |  ");
+System.out.println("   | \\__/ |    ");
+System.out.println("    |    |     ");
+System.out.println("     |  |      ");
+
+}
+}
